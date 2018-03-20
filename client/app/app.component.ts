f91f4acb9950281f88387f745a72cfe0ce280b02
vijaychauhanssn <vijaychauhanssn@gmail.com>
2018-03-20 16:36:46 +0530

    upadted

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent implements AfterViewChecked {
   constructor(public auth: AuthService,
               private changeDetector: ChangeDetectorRef) { }
 
-  // This fixes: https://github.com/DavideViolante/Angular-Full-Stack/issues/105
+  // This fixes:  https://github.com/vijaychauhanssn/angular2-full-stack/issues/105
   ngAfterViewChecked() {
     this.changeDetector.detectChanges();
   }
